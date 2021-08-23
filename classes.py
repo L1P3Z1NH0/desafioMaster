46d6629b847b97e5ddd55571e05a3c3ca6c455a7
L1P3Z1NH0 <felipecarrapeiro@hotmail.com>
2021-08-23 14:45:25 -0300

    ex de logica versao final

--- classes.py
+++ classes.py
@@ -3,29 +3,45 @@ class Servers:
     def __init__(self, ttask, umax):
         self.ttask = ttask
         self.umax = umax
-        self.servers = {}
-        self.id = 0
-        self.contServer = 0
-        self.contMax = 0
+        self.servers = []
+        self.count_server = 0
+        self.count_max = 0
+        self.count_created = 0
 
     def insert_user(self, ttask, num_users):
+        self.count_created = 0
         for i in range(num_users):
-            if f'Servidor {self.contServer}' not in self.servers or self.contMax >= self.umax:
-                self.contMax = 0
-                self.contServer += 1
-                self.id += 1
-                self.contMax += 1
-                self.servers[f'Servidor {self.contServer}'] = {self.id: ttask + 1}
+            if len(self.servers) == 0 or self.count_max >= self.umax:
+                self.count_max = 0
+                self.count_created += 1
+                self.count_server += 1
+                self.count_max += 1
+                self.servers.append([ttask + 1])
             else:
-                self.id += 1
-                self.contMax += 1
-                self.servers[f'Servidor {self.contServer}'].update({self.id: ttask + 1})
+                self.count_max += 1
+                self.servers[-1].append(ttask + 1)
 
-    def task_control(self):
-        for serverK, serverV in self.servers.items():
-            for userV in list(serverV.keys()):
-                serverV[userV] -= 1
+        for serverI, serverV in enumerate(self.servers):
+            subtract_all = [i - 1 for i in self.servers[serverI]]
+            self.servers[serverI] = subtract_all
 
-                if serverV[userV] <= 0:
-                    print(f'id= {userV} foi excluido')
-                    serverV.pop(userV)
+            filtered_list = [i for i in self.servers[serverI] if i]
+            self.servers[serverI] = filtered_list
+
+            if not self.servers[serverI]:
+                self.count_server -= 1
+
+        if [] in self.servers:
+            remove_empty = [i for i in self.servers if i]
+            self.servers = remove_empty
+
+
+        # for i, v in enumerate(self.servers):
+        #     if len(self.servers[i]) < self.umax and len(self.servers[i]) != len(self.servers[-1]):
+        #         self.servers[i].append(self.servers[-1][-1])
+        #         del self.servers[-1][-1]
+        #
+        #     elif len(self.servers[i]) < self.umax and len(self.servers[i - 1]) == len(self.servers[-1]) \
+        #             and len(self.servers) > 1:
+        #         self.servers[i].append(self.servers[-1][-1])
+        #         del self.servers[-1][-1]
--- main.py
+++ main.py
@@ -4,12 +4,19 @@ ttask = input('Insira o número de "ticks" para finalizar uma tarefa: ')
 ttask = int(ttask)
 umax = input('Insira o número máximo de usuários por servidor: ')
 umax = int(umax)
+preco = 0
 
 c1 = Servers(ttask, umax)
 for i in range(10):
     num_users = input('Quantos usuários irão se conectar ?')
     num_users = int(num_users)
     c1.insert_user(ttask, num_users)
-    c1.task_control()
     print(c1.servers)
-    print(f'{c1.contServer} servidores')
+    if c1.count_created >= 1:
+        print(f'{c1.count_created} servidores foram criados')
+    else:
+        print('Nenhum servidor foi criado')
+    print(f'{c1.count_server} servidores')
+    preco += 1 * c1.count_server
+    print(f'Preco atual R${preco}')
+
